da683b0cf6da64ddcf26cb92e979335a55bb5b63
Nidde Nedelius <github@nidde.nu>
2023-08-22 21:20:33 +0200

    update gallery layout and functionality

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -11,23 +11,49 @@
 @section('content')
     <div class="gallery-container" id="gallery-container">
         <div id="viewer" class="viewer">
-            <div class="viewer-item">
-                <div class="sd-image">
-                    <a href="{{ $image }}" target="_blank">
-                        <img src="{{ $lossy }}" loading="lazy" />
-                    </a>
+            <div class="viewer-image">
+                <img src="{{ $lossy }}" loading="lazy" id="image" data-image="{{ $lossy }}" />
+                <div class="image-buttons">
+                    <button class="image-button" id="infoButton" data-image="{{ $image }}"><i
+                            class="fa-solid fa-circle-info"></i></button>
+                    <button class="image-button" id="downloadButton" data-link="{{ $image }}"><i
+                            class="fa-solid fa-download"></i></button>
+                </div>
+                <div class="sd-info" id="sd-info">
+                    <div class="parameter">
+                        <div class="label">Prompt:</div>
+                        <div class="prompt" id="prompt"></div>
+                    </div>
+                    <div class="parameter">
+                        <div class="label">Negative prompt:</div>
+                        <div class="prompt" id="negative_prompt"></div>
+                    </div>
+                    <div class="settings">
+                        <div class="settings-item">
+                            <div class="label">Model:</div>
+                            <div class="setting" id="model"></div>
+                        </div>
+                        <div class="settings-item">
+                            <div class="label">Seed:</div>
+                            <div class="setting" id="seed"></div>
+                        </div>
+                        <div class="settings-item">
+                            <div class="label">Steps:</div>
+                            <div class="setting" id="steps"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="gallery">
             <div class="gallery-thumbnails">
-            @foreach ($thumbnails as $thumbnail)
-                <div class="gallery-item">
-                    <button class="galleryButton" data-image="{{ $thumbnail }}">
-                        <img src="{{ $thumbnail }}" loading="lazy" />
-                    </button>
-                </div>
-            @endforeach
+                @foreach ($thumbnails as $thumbnail)
+                    <div class="gallery-item">
+                        <button class="galleryButton" data-image="{{ $thumbnail }}">
+                            <img src="{{ $thumbnail }}" loading="lazy" />
+                        </button>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
